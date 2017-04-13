64fe7d5ea7eda083f90625b7941c2d46e8354d42
Kapya <kapsakala@gmail.com>
2017-04-13 07:29:39 +0200

    replaced pass with env variable

--- reporting/send_report.sh
+++ reporting/send_report.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ~/reports
 #check reports folder for file most recently created from monthend or alldata and send to google server
-sshpass -p "*ismahan" scp `ls ~/reports -t | head -n1` edulution@130.211.93.74:/home/edulution/reports
+sshpass -p $SSHPASS scp `ls ~/reports -t | head -n1` edulution@130.211.93.74:/home/edulution/reports
 # if connection lost the script will exit with status 1 and output error message
 if [ "$?" = "0" ]; then
 	echo Report submitted successfully!
