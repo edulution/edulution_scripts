c2515ecc282060e2da2348dd67b8a24efc679583
Kapya <kapsakala@gmail.com>
2017-04-27 10:50:27 +0200

    added comments, show date of most recent commit

--- identify/identify.py
+++ identify/identify.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python
 import sqlite3
-#import version
 from os.path import expanduser
-
+import subprocess
 
 ka_database = expanduser("~/.kalite/database/data.sqlite")
-id_file = expanduser("~/.scripts/identify/id.txt")
+#get top 3 lines of git log to get id and date of most recent commit
+latest_update = subprocess.check_output("cd ..;git log | head -n 3", shell=True)
 
-if __name__ == '__main__':
-    db = sqlite3.connect(ka_database)
-    c = db.cursor()
-    results = c.execute(""" 
-select d.name from securesync_device d
+# connect to kalite database file
+db = sqlite3.connect(ka_database)
+c = db.cursor()
+
+#get device name and version from kalite database
+results = c.execute(""" 
+select d.name,version from securesync_device d
 join securesync_devicemetadata s
 where s.device_id = d.id
 and s.is_own_device = 1 """)
-    for row in results:
+for row in results:
        device = row[0]
+       version = row[1]
 
-    version = max([tuple(map(int, v.split('.'))) for v in version.VERSION_INFO.keys()])
-    print 'Device:', device
-    print 'Version:', '.'.join(map(str, version))
+#Print device name, version, and most recent commit to screen
+print 'Device:', device
+print 'Version:', version
+print 'Latest Update:', latest_update
 
-    with open(id_file) as idfile:
-       lines = map(lambda s: s.strip(), idfile.readlines())
-       for row in lines:
-           if len(row) > 0:
-              print row
