5e045c136612eec8f6f51a56687984c5ed3e54c8
Kapya <kapsakala@gmail.com>
2017-05-03 11:20:58 +0200

    fixed users query and date formattig

--- reporting/monthend.R
+++ reporting/monthend.R
@@ -14,9 +14,9 @@ dbfile <- "~/.kalite/database/data.sqlite"
 conn <- dbConnect(sqlite, dbfile)
 
 #get users
-users_query <- dbSendQuery(conn,"SELECT * FROM securesync_facilityuser")
+users_query <- dbSendQuery(conn,"SELECT * FROM securesync_facilityuser where(deleted == 0 and is_teacher == 0)")
   #filter out deleted users and coaches, then select only columns needed for joins later in script
-users <- dbFetch(users_query) %>% filter(deleted == 0 && is_teacher == 0) %>% select(id,username,first_name,last_name,group_id,facility_id)
+users <- dbFetch(users_query) %>% select(id,username,first_name,last_name,group_id,facility_id)
 
 #get facilities - All centres have only 1 facility on centralserver except CI
 facility_query <- dbSendQuery(conn,"SELECT * FROM securesync_facility")
@@ -67,11 +67,11 @@ monthend <- function(year_month) {
   # with variable from above date, parse into date and get last day in month then convert into proper date format
   upper_limit <- as.Date(timeLastDayInMonth(strftime(upper_limit,"%d-%m-%y"),format = "%y-%m-%d"))
   # Need to get end of month in standard format before chopping it up for grepping. Need it for monthend column in final csv file
-  mthnd <- upper_limit
+  monthend_column <- strftime(upper_limit,format = "%d-%m-%y")
   upper_limit <- substring(upper_limit,1,7)
   exercises_per_user <- main_exerciselog %>% filter(grepl(upper_limit, latest_activity_timestamp) || grepl(upper_limit, completion_timestamp)) %>% group_by(user_id) %>% summarize(exercises_attempted = n(), total_exercises = sum(complete))
   videos_per_user <- main_videolog %>% filter(grepl(upper_limit, latest_activity_timestamp)) %>% group_by(user_id) %>% summarize(total_videos = n())
-  complete_summary <- main_userlogsummary %>% filter(grepl(upper_limit,last_activity_datetime)) %>% group_by(user_id) %>% summarise(total_hours = sum(total_seconds)/3600, total_logins = n(), last_active_date = max(as.Date(last_activity_datetime))) %>% right_join(users, by = c("user_id" = "id")) %>% left_join(exercises_per_user, by = "user_id") %>% left_join(videos_per_user, by = "user_id") %>% left_join(facilities, by = c("facility_id" = "id")) %>% left_join(groups, by = c("group_id" = "id"))%>% mutate(month_end=rep(strftime(mthnd,format = "%d-%m-%y")))
+  complete_summary <- main_userlogsummary %>% filter(grepl(upper_limit,last_activity_datetime)) %>% group_by(user_id) %>% summarise(total_hours = sum(total_seconds)/3600, total_logins = n(), last_active_date = max(as.Date(last_activity_datetime))) %>% right_join(users, by = c("user_id" = "id")) %>% left_join(exercises_per_user, by = "user_id") %>% left_join(videos_per_user, by = "user_id") %>% left_join(facilities, by = c("facility_id" = "id")) %>% left_join(groups, by = c("group_id" = "id"))%>% mutate(month_end=rep(monthend_column))
   rpt <- complete_summary %>% select(user_id,first_name,last_name,username,name.y,total_logins,total_hours,total_exercises,total_videos,month_end,exercises_attempted,name.x,last_active_date) %>% rename(centre = name.x, group = name.y, id = user_id)
   write.csv(rpt, file = generate_filename("monthend_",year_month) ,col.names = FALSE, row.names = FALSE,na="0")
   system("echo Report extracted successfully!")
