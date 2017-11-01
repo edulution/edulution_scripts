48b82316061568846e09dd94db4d1dad4add8712
techZM <techzm@edulution.org>
2017-11-01 12:47:15 +0200

    Removed integrity check
    Integrity check was causing problems with large databases (>1GB) or left db locked when interrupted

--- backupdb/backup.sh
+++ backupdb/backup.sh
@@ -4,7 +4,7 @@ database_path=~/.kalite/database/data.sqlite
 database_name=$(sqlite3 $database_path "SELECT d.name FROM securesync_device d JOIN securesync_devicemetadata s WHERE s.device_id = d.id AND s.is_own_device = 1")
 
 # Lock database and do integrity check, then unlock
-integrity_check=$(sqlite3 $database_path "begin immediate;pragma integrity_check;rollback")
+#integrity_check=$(sqlite3 $database_path "begin immediate;pragma integrity_check;rollback")
 
 file_extension=".sqlite"
 
@@ -18,14 +18,14 @@ backup_name=${database_name}_${timestamp}${file_extension}
 
 # Append database name to the string "db_errorlog" to make errolog file
 # Check if file exists and
-filename=${database_name}_"db_error.log"
+#filename=${database_name}_"db_error.log"
 
-if [ ! -e "~/.scripts/backupdb/$filename" ] ; then
-    touch ~/.scripts/backupdb/$filename
-fi
+#if [ ! -e "~/.scripts/backupdb/$filename" ] ; then
+#    touch ~/.scripts/backupdb/$filename
+#fi
 
 # Log result of integrity check to errorlog file
-echo ${complete_timestamp}-${integrity_check} >> ~/.scripts/backupdb/$filename
+#echo ${complete_timestamp}-${integrity_check} >> ~/.scripts/backupdb/$filename
 
 # create backup file using variables above
 echo "Checking Database integrity and creating backup"
