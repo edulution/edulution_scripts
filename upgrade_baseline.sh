5f3ccd09190a880a0e8cfd8ed3fb1444e147f1cf
Kapya <kapsakala@gmail.com>
2018-09-14 10:26:26 +0200

    pull latest changes from baseline system when submitting monthend report

---
+++ upgrade_baseline.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#Switch to home directory
+cd ~
+
+# Check if client script exists 
+test -f ~/.baseline_testing/startup_script
+if [ "$?" = "0" ]; then
+	echo "Baseline Testing already configured"
+	cd ~/.baseline_testing/
+	echo "Pulling latest changes to Baseline system..."
+	git pull
+	./setup.sh
+else
+	echo "Baseline system not configured correctly or missing from system"
+	rm -rf ~/.baseline_testing
+	echo "Cloning repository..."
+	git clone https://github.com/techZM/offline_testing.git .baseline_testing
+	cd ~/.baseline_testing/
+	./setup.sh
+fi
