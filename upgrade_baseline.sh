f9470bb7a56b16627927df6d87c3f2a7323de3f0
Kapya <kapsakala@gmail.com>
2018-12-18 11:52:54 +0200

    point to correct script when upgrading baseline

--- upgrade_baseline.sh
+++ upgrade_baseline.sh
@@ -4,18 +4,18 @@
 cd ~
 
 # Check if client script exists 
-test -f ~/.baseline_testing/startup_script
+test -f ~/.baseline_testing/scripts/startup_script
 if [ "$?" = "0" ]; then
 	echo "Baseline Testing already configured"
 	cd ~/.baseline_testing/
 	echo "Pulling latest changes to Baseline system..."
 	git pull origin master
-	./setup.sh
+	./scripts/setup.sh
 else
 	echo "Baseline system not configured correctly or missing from system"
 	rm -rf ~/.baseline_testing
 	echo "Cloning repository..."
 	git clone https://github.com/techZM/offline_testing.git .baseline_testing
 	cd ~/.baseline_testing/
-	./setup.sh
+	./scripts/setup.sh
 fi
