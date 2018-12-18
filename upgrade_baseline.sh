682a0e622189059fd73be5a3a1d656dba813e6d7
Kapya <kapsakala@gmail.com>
2018-12-18 12:17:50 +0200

    make baseline setup script executable

--- upgrade_baseline.sh
+++ upgrade_baseline.sh
@@ -10,12 +10,12 @@ if [ "$?" = "0" ]; then
 	cd ~/.baseline_testing/
 	echo "Pulling latest changes to Baseline system..."
 	git pull origin master
-	./scripts/setup.sh
+	~/.baseline_testing/scripts/setup.sh
 else
 	echo "Baseline system not configured correctly or missing from system"
 	rm -rf ~/.baseline_testing
 	echo "Cloning repository..."
 	git clone https://github.com/techZM/offline_testing.git .baseline_testing
 	cd ~/.baseline_testing/
-	./scripts/setup.sh
+	~/.baseline_testing/scripts/setup.sh
 fi
