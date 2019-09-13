6f74935099ceb02213e21af8a3f2172480830daf
Kapya <kapsakala@gmail.com>
2019-09-13 16:22:21 +0200

    added script to import channels from /opt directory

--- identify/identify.py
+++ identify/identify.py
@@ -1,28 +1,29 @@
-#!/usr/bin/env python
-import sqlite3
-from os.path import expanduser
+import os
+import psycopg2
+import pkg_resources
 import subprocess
 
-ka_database = expanduser("~/.kalite/database/data.sqlite")
-#get top 3 lines of git log to get id and date of most recent commit
-latest_update = subprocess.check_output("cd ~/.scripts;git log | head -n 3", shell=True)
+# get database connection credentials from environment variables
+dbname = os.environ['KOLIBRI_DATABASE_NAME']
+dbpass = os.environ['KOLIBRI_DATABASE_PASSWORD']
+dbuser = os.environ['KOLIBRI_DATABASE_USER']
+dbhost = os.environ['KOLIBRI_DATABASE_HOST']
+dbport = os.environ['KOLIBRI_DATABASE_PORT']
 
-# connect to kalite database file
-db = sqlite3.connect(ka_database)
-c = db.cursor()
+# create a connection to the database using the credentials gathered
+conn = psycopg2.connect(dbname = dbname,user=dbuser,password=dbpass,host=dbhost,port=dbport)
+curr = conn.cursor()
+curr.execute("""select name from kolibriauth_collection where id = (select default_facility_id from device_devicesettings)""")
+res = curr.fetchall()
+facility_name = res[0][0]
 
-#get device name and version from kalite database
-results = c.execute(""" 
-select d.name,version from securesync_device d
-join securesync_devicemetadata s
-where s.device_id = d.id
-and s.is_own_device = 1 """)
-for row in results:
-       device = row[0]
-       version = row[1]
+# get kolibri version
+kolibri_version = pkg_resources.get_distribution("kolibri").version
 
-#Print device name, version, and most recent commit to screen
-print 'Device:', device
-print 'Version:', version
-print 'Latest Update:', latest_update
+# get top 3 lines of git log to get latest scripts update
+latest_update = subprocess.check_output("cd ~/.scripts;git log | head -n 4", shell=True)
 
+# print out the facility name, kolibri version, and latest update
+print 'Device:', facility_name
+print 'Version:', kolibri_version
+print 'Last Updated:', latest_update
