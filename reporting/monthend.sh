c5d23ec4eca02e6ea99803a9ceac48eb85f8c543
Kapya <kapsakala@gmail.com>
2019-09-13 17:39:01 +0200

    changed monthend script to check if postgresql server is running before extracting data. removed unused scripts

--- reporting/monthend.sh
+++ reporting/monthend.sh
@@ -34,22 +34,18 @@ git pull origin kolibri > /dev/null
 #Do silent upgrade of all scripts
 ./upgrade_silent.sh > /dev/null
 
-#check if database file exists before extracting reports
-test -f ~/.kalite/database/data.sqlite
-#if db file exists then extraction and submission begin. If not, will output error message to contact support
-if [ "$?" = "0" ]; then
+# check if postgresql is running before attempting to extract a report
+ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
+result=$(echo $ps_out | grep "$1")
+
+if [[ "$result" != "" ]];then
 	if (echo $1 |\
     egrep '^(1[0-2]|0[0-9])[-/][0-9]{2}' > /dev/null
 	); then
-	   echo Stopping ka lite server 
-	   sudo service ka-lite stop > /dev/null
+	   echo Stopping Kolibri server 
+	   python -m kolibri stop > /dev/null
 	   sudo service nginx stop > /dev/null
        echo "${green}Extracting data for month $1${reset}"
-       echo Checking and fixing students with abnormal hours
-       ~/.scripts/reporting/fix_crazy/fixcrazy       
-
-       echo Deleting learners from Zarchive group
-       ~/.scripts/config/delete_zarchive
 
        echo Beginning report extraction.....
        # fetch the first argument given on the command line and use it as an argument to the Rscript
@@ -60,14 +56,9 @@ if [ "$?" = "0" ]; then
        # submit baseline tests for the selected month
        ~/.baseline_testing/scripts/reporting/baseline.sh $1
 
-       #remove autocomplete function
-       ~/.scripts/config/remove_autocomplete.sh
- 	
        # Pull latest changes to baseline system
        ~/.scripts/upgrade_baseline.sh
        
-       # Remote support setup
-       #~/.scripts/remote_support_setup.sh > /dev/null
    else 
        echo "${red}Please enter a valid year and month e.g 02-17${reset}"
        exit 1 
