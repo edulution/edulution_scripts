e48a069b4f1a0986c227cd958b9b937f4da162a9
Kapya <kapsakala@gmail.com>
2019-09-15 11:01:14 +0200

    script to restart kolibri

---
+++ restart_kolibri.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# export the python path
+PYTHONPATH=/usr/lib/python2.7
+
+# stop the kolibri server
+python -m kolibri stop
+# stop all processes running on port 8080
+fuser -k 8080/tcp
+
+python -m kolibri start
