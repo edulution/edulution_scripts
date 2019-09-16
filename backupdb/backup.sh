0075ddc29b5dd5a3ec2e370b57bacc21aa6ceec9
Kapya <>
2019-09-16 10:18:23 +0200

    corrected backup scripts. now working

--- backupdb/backup.sh
+++ backupdb/backup.sh
@@ -9,12 +9,18 @@ timestamp=$(date +"%Y%m%d")
 # get database name using direct query on terminal
 database_name=$(PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD psql -d $KOLIBRI_DATABASE_NAME -U $KOLIBRI_DATABASE_USER -h $KOLIBRI_DATABASE_HOST -p $KOLIBRI_DATABASE_PORT -t -c "select name from kolibriauth_collection where id = (select default_facility_id from device_devicesettings);")
 
-# Derive backup nam
+#remove leading whitespace
+database_name="${database_name##*( )}"
+
+#remove trailing whitespace
+database_name="${database_name%%*( )}"
+
+# Derive backup name by combining the dataabse name, timestamp and file extension
 backup_name=${database_name}_${timestamp}${file_extension}
 
 # Create database backup using credentials from environment variables 
 echo "Creating database backup"
-PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD pg_dump $KOLIBRI_DATABASE_NAME -U $KOLIBRI_DATABASE_USER -h $KOLIBRI_DATABASE_HOST -p $KOLIBRI_DATABASE_PORT -Fc > ~/.backups/$backup_name
+PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD pg_dump $KOLIBRI_DATABASE_NAME -U $KOLIBRI_DATABASE_USER -h $KOLIBRI_DATABASE_HOST -p $KOLIBRI_DATABASE_PORT -Fc > ~/backups/"$backup_name"
 
 # Call script to remove backups older than 40 days
 ~/.scripts/backupdb/remove_old_backups.sh
