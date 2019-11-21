f82f4f5e6ec71c420f5e71b5be8ec5eeeec351ed
Kapya <kapsakala@gmail.com>
2019-11-21 18:02:43 +0200

    import channel before import content

--- config/import_channels.sh
+++ config/import_channels.sh
@@ -34,17 +34,17 @@ python -m kolibri manage importchannel -- disk 1d8f1428da334779b95685c4581186c4 
 python -m kolibri manage importcontent -- disk 1d8f1428da334779b95685c4581186c4 /opt/KOLIBRI_DATA/
 
 # Bravo A
-python -m kolibri manage importcontent -- disk 57995474194c4068bfed1ee16108093f /opt/KOLIBRI_DATA/
+python -m kolibri manage importchannel -- disk 57995474194c4068bfed1ee16108093f /opt/KOLIBRI_DATA/
 python -m kolibri manage importcontent -- disk 57995474194c4068bfed1ee16108093f /opt/KOLIBRI_DATA/
 
 # Bravo B
-python -m kolibri manage importcontent -- disk b7214b921fd94a1cb758821919bcd3e0 /opt/KOLIBRI_DATA/
+python -m kolibri manage importchannel -- disk b7214b921fd94a1cb758821919bcd3e0 /opt/KOLIBRI_DATA/
 python -m kolibri manage importcontent -- disk b7214b921fd94a1cb758821919bcd3e0 /opt/KOLIBRI_DATA/
 
 # Bravo C
-python -m kolibri manage importcontent -- disk 5aee4435135b4039a3a824d96f72bfcb /opt/KOLIBRI_DATA/
+python -m kolibri manage importchannel -- disk 5aee4435135b4039a3a824d96f72bfcb /opt/KOLIBRI_DATA/
 python -m kolibri manage importcontent -- disk 5aee4435135b4039a3a824d96f72bfcb /opt/KOLIBRI_DATA/
 
 # Bravo D
-python -m kolibri manage importcontent -- disk 98ab8048107545da92e3394409955526 /opt/KOLIBRI_DATA/
+python -m kolibri manage importchannel -- disk 98ab8048107545da92e3394409955526 /opt/KOLIBRI_DATA/
 python -m kolibri manage importcontent -- disk 98ab8048107545da92e3394409955526 /opt/KOLIBRI_DATA/
