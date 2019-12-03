8c3fea7d444468a42a29bf7045db1fcf72d83713
Kapya <kapsakala@gmail.com>
2019-12-03 10:40:04 +0200

    SQL to reorder channels

---
+++ config/reorder_channels.sql
@@ -0,0 +1,13 @@
+/*SQL to reorder channels in case script does not work*/
+update content_channelmetadata set "order" = 1 where id = '3d6c9d72-a2e0-47d4-b7a0-ed20699e1b1f'; /*Pre Alpha Alpha*/
+update content_channelmetadata set "order" = 2 where id = '6380a6a9-8a4c-4b26-8b31-47ad1c7ada13'; /*Pre Alpha B*/
+update content_channelmetadata set "order" = 3 where id = '20113bf1-ba07-4e08-bcc7-faaca03ade8a'; /*Pre Alpha C*/
+update content_channelmetadata set "order" = 4 where id = '1700bf9e-7109-4857-abf3-6c04a1963004'; /*Pre Alpha D*/
+update content_channelmetadata set "order" = 5 where id = '8784b9f7-8d58-4273-aff5-79b246529215'; /*Alpha A*/
+update content_channelmetadata set "order" = 6 where id = 'cc805378-86cb-498e-b564-242f44c87723'; /*Alpha B*/
+update content_channelmetadata set "order" = 7 where id = '7035e792-1ddf-489f-ad45-44c814a199fb'; /*Alpha C*/
+update content_channelmetadata set "order" = 8 where id = '1d8f1428-da33-4779-b956-85c4581186c4'; /*Alpha D*/
+update content_channelmetadata set "order" = 9 where id = '57995474-194c-4068-bfed-1ee16108093f'; /*Bravo A*/
+update content_channelmetadata set "order" = 10 where id = 'b7214b92-1fd9-4a1c-b758-821919bcd3e0'; /*Bravo B*/
+update content_channelmetadata set "order" = 11 where id = '5aee4435-135b-4039-a3a8-24d96f72bfcb'; /*Bravo C*/
+update content_channelmetadata set "order" = 12 where id = '98ab8048-1075-45da-92e3-394409955526'; /*Bravo D*/
