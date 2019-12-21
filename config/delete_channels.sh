74ae79e8c24b63cecf69815d924dadb09791fcab
Kapya <kapsakala@gmail.com>
2019-12-21 15:32:25 +0200

    script to delete all numeracy channels in case of emergency

---
+++ config/delete_channels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Importing numeracy playlists"
+
+# delete channels
+
+python -m kolibri manage deletechannel 3d6c9d72-a2e0-47d4-b7a0-ed20699e1b1f
+python -m kolibri manage deletechannel 6380a6a9-8a4c-4b26-8b31-47ad1c7ada13
+python -m kolibri manage deletechannel 20113bf1-ba07-4e08-bcc7-faaca03ade8a
+python -m kolibri manage deletechannel 1700bf9e-7109-4857-abf3-6c04a1963004
+python -m kolibri manage deletechannel 8784b9f7-8d58-4273-aff5-79b246529215
+python -m kolibri manage deletechannel cc805378-86cb-498e-b564-242f44c87723
+python -m kolibri manage deletechannel 7035e792-1ddf-489f-ad45-44c814a199fb
+python -m kolibri manage deletechannel 1d8f1428-da33-4779-b956-85c4581186c4
+python -m kolibri manage deletechannel 57995474-194c-4068-bfed-1ee16108093f
+python -m kolibri manage deletechannel b7214b92-1fd9-4a1c-b758-821919bcd3e0
+python -m kolibri manage deletechannel 5aee4435-135b-4039-a3a8-24d96f72bfcb
+python -m kolibri manage deletechannel 98ab8048-1075-45da-92e3-394409955526
