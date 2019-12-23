737ac8c4c574a338bb90d0b9cd473f4fc1b72094
Kapya <kapsakala@gmail.com>
2019-12-23 16:52:32 +0200

    make alldata script similar to monthend script but without month filter

--- reporting/alldata.R
+++ reporting/alldata.R
@@ -142,37 +142,87 @@ generate_filename <- function(report,date){
 
 # Function to get all data in db from beginning of time until month that user specifies
 alldata <- function(year_month) {
-  #with user input from command line, create complete date by prefixing with 01
+    #with user input from command line, create complete date by prefixing with 01
   upper_limit <- paste("01-",year_month,sep="")
   #regular expression to check if the user input is a valid month and year, and in the form mm-yy
   regexp <-'((?:(?:[0-2]?\\d{1})|(?:[3][01]{1}))[-:\\/.](?:[0]?[1-9]|[1][012])[-:\\/.](?:(?:\\d{1}\\d{1})))(?![\\d])'
   #check if its a valid date and correct number of characters. stops the program if input not fit
-  if(!(grepl(pattern = regexp,x=upper_limit,perl = TRUE)) | (nchar(upper_limit) > 8)) stop("Please enter a valid month and year mm-yy e.g '12-16'")
-  # with variable from above date, parse into date and get last day in month then convert into proper date format
-  upper_limit <- as.Date(timeLastDayInMonth(strftime(upper_limit,"%d-%m-%y"),format = "%y-%m-%d"))
-  # get exercises per user - group userlog rows by user_id then get max last active date and summarize total exercises, exercises complete
-  #exercises_per_user <- main_exerciselog %>% filter(upper_limit >= as.Date(latest_activity_timestamp)) %>% group_by(user_id) %>% summarize(exercises_attempted = n(), total_exercises = sum(complete))
-  exercises_per_user <- main_exerciselog %>% group_by(user_id) %>% summarize(exercises_attempted = n(), total_exercises = sum(complete))
-  #almost the same as query above, except we dont care about anything except total videos watched
-  #videos_per_user <- main_videolog %>% filter(upper_limit >= as.Date(latest_activity_timestamp)) %>% group_by(user_id) %>% summarize(total_videos = n())
-  videos_per_user <- main_videolog %>% filter(total_seconds_watched > 180) %>% group_by(user_id) %>% summarize(total_videos = n())
-  #first summarizes user activity from userlog summary, then produces complete aggregation of data after joining users(r_join),exercises per user,videos per user, facility, group
-  complete_summary <- main_userlogsummary %>% group_by(user_id) %>% summarise(total_hours = sum(total_seconds)/3600, total_logins = n(), last_active_date = max(as.Date(last_activity_datetime))) %>% right_join(users, by = c("user_id" = "id")) %>% left_join(exercises_per_user, by = "user_id") %>% left_join(videos_per_user, by = "user_id") %>% left_join(facilities, by = c("facility_id" = "id")) %>% left_join(groups, by = c("group_id" = "id")) %>% mutate(month_end=rep(upper_limit))
-  # selecting fields to be written to csv file and changing column names appropriately
-  rpt <- complete_summary %>% select(user_id,first_name,last_name,username,name.y,total_logins,total_hours,total_exercises,total_videos,month_end,exercises_attempted,last_active_date) %>% rename(group = name.y, id = user_id) %>% mutate(centre = rep(device_name), month_active = ifelse(total_hours>0, 1, 0), module=rep("numeracy"))
-   # Set total exercises and total videos to 0 if total hours is 0
-  rpt <- rpt %>% mutate(total_exercises=replace(total_exercises, total_hours == 0, 0)) %>% mutate(total_videos=replace(total_videos, total_hours == 0, 0))
+  if(!(grepl(pattern = regexp,x=upper_limit,perl = TRUE)) | (nchar(upper_limit) > 8)) stop("Please enter a valid month and year mm-yy e.g 02-17")
+  
+  # get month start and month end as correctly formatted strings
+  month_end <- as.Date(timeLastDayInMonth(strftime(upper_limit,"%d-%m-%y"),format = "%y-%m-%d"))
+  month_start <- as.Date(timeFirstDayInMonth(strftime(upper_limit,"%d-%m-%y"),format = "%y-%m-%d"))
+  
+  #get total time spent by each user between month start and month end
+  time_spent_by_user <- content_sessionlogs %>% group_by(user_id) %>% summarize(total_hours = sum(time_spent)/3600)
+  
+  # get the number of distinct days a user logeed in using the start_timestamp date only
+  logins_by_user <- content_sessionlogs %>% distinct(user_id,start_date_only) %>% group_by(user_id) %>% summarize(total_logins = n())
+  
+  # get the total number of completed exercises and videos between month start and month end
+  completed_ex_vid_count <- content_sessionlogs %>% filter(progress >= 0.99) %>% group_by(user_id,kind) %>% summarize(count = n())
+  
+  # transpose the rows into columns by user_id
+  # exercise and video counts become columns
+  completed_ex_vid_count <- tidyr::spread(completed_ex_vid_count,kind,count)
+  
+  # rename the columns to read total_exercises, total_videos
+  if("video" %in% colnames(completed_ex_vid_count)){
+    completed_ex_vid_count <- completed_ex_vid_count %>% rename(total_exercises = exercise, total_videos = video)
+  }
+
+  else{
+    completed_ex_vid_count <- completed_ex_vid_count %>% rename(total_exercises = exercise) %>% mutate(total_videos = 0)
+  }
+  
+  # get total time spent by channel
+  time_by_channel <- content_sessionlogs %>% group_by(user_id,channel_id) %>% summarise(total_time = sum(time_spent)/3600) %>% spread(channel_id, total_time)
+  # this produces a data frame with time spent on each channel as a separate column with the channel id as the column name
+  
+  # change column names which are channel_ids from channel_ids to readable course names
+  # using the named vector created outside the function
+  names(time_by_channel) <- c("user_id",recode(names(time_by_channel)[-1],!!!course_name_id))
+  
+  # get total_progress by channel_id (no need to filter for the month requested)
+  prog_by_user_by_channel <- content_sessionlogs %>% group_by(user_id,channel_id,content_id) %>% summarise(max_prog = max(progress)) %>% group_by(user_id,channel_id) %>% summarise(total_prog=sum(max_prog))
+  
+  # join total prog by channel to number of items by channel. used to get percent progress in channel
+  prog_by_user_by_channel <- prog_by_user_by_channel %>% left_join(num_contents_by_channel)
+  
+  # create a column for the percent progress by channel
+  prog_by_user_by_channel <- prog_by_user_by_channel %>% mutate(pct_progress = total_prog/total_items)
+  
+  # get rid of the columns for total prog and total_items then turn the progress for each channel into a separate column
+  prog_by_user_by_channel <- prog_by_user_by_channel %>% select(-c(total_prog,total_items)) %>% spread(channel_id, pct_progress)
+  
+  # change the column names to be the name of the channel + _progress
+  names(prog_by_user_by_channel) <- c("user_id",recode(names(prog_by_user_by_channel)[-1],!!!course_name_id_progress))
+  
+  # everything together to make a complete report
+  rpt <- users %>% left_join(time_spent_by_user,by=c("id"="user_id")) %>% left_join(completed_ex_vid_count,by=c("id"="user_id")) %>% left_join(logins_by_user,by=c("id"="user_id")) %>% left_join(time_by_channel,by=c("id"="user_id")) %>% left_join(prog_by_user_by_channel,by=c("id"="user_id")) %>% left_join(learners_and_groups,by=c("id"="user_id"))
+  
+  # add month active, module, and centre by mutation
+  rpt <- rpt %>% mutate(month_active = ifelse(total_hours>0, 1, 0), module=rep("numeracy"), centre=rep(device_name))
+
+  # Set total exercises and total videos to 0 if total hours is 0
+  rpt <- rpt %>% mutate(total_exercises=replace(total_exercises, total_hours == 0, 0)) %>% mutate(total_videos=replace(total_videos, total_hours == 0, 0), month_end=rep(strftime(month_end,"%Y-%m-%d")))
+
+  #derive the first name and last name columns using helper functions
+  rpt$first_name <- sapply(rpt$full_name,get_first_name)
+  rpt$last_name <- sapply(rpt$full_name,get_last_name)
 
   # convert id column from uuid to character string
   rpt <- rpt %>% mutate(id = str_replace_all(id,'-',''))
 
-  # create csv file with filename generated by based on name of report and user input
-  write.csv(rpt, file = generate_filename("data.all_",year_month) ,col.names = FALSE, row.names = FALSE,na="0")
-  #print success messgae
+  #reorder columns. put familiar columns first
+  rpt <- rpt %>% select(c(id, first_name, last_name, username, group, total_hours, total_exercises, total_videos, month_end, centre, last_login, month_active, module, total_logins),everything())
+  
+  #Write report to csv
+  write.csv(rpt, file = generate_filename("alldata_",year_month) ,col.names = FALSE, row.names = FALSE,na="0")
   system("echo Report extracted successfully!")
-  #dont save workspace
   quit(save="no")
 }
+
 
 # fetch user input from the command line
 input<- commandArgs(TRUE)
