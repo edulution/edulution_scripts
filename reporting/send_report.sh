3097fda86363a23ac088c27f31cfc3d3d5defdf7
Kapya <kapsakala@gmail.com>
2019-12-23 16:58:10 +0200

    change directory to .reports in home directory

--- reporting/send_report.sh
+++ reporting/send_report.sh
@@ -25,7 +25,7 @@ export exit_bold_mode=`tput rmso`
 
 
 # Go into reports folder
-cd ~/reports
+cd ~/.reports
 #check reports folder for file most recently created from monthend or alldata and send to google server
 sshpass -p $SSHPASS scp `ls ~/.reports -t | head -n1` edulution@130.211.93.74:/home/edulution/reports
 # if connection lost the script will exit with status 1 and output error message
