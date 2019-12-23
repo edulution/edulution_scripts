7cf7be14345ed4ce2b5b14cda8d68e79c8a5fa1c
Kapya <kapsakala@gmail.com>
2019-12-23 16:51:46 +0200

    [wip] modifying alldata script to new database connection

--- reporting/alldata.R
+++ reporting/alldata.R
@@ -1,64 +1,138 @@
 # Import tables and generate filename script
-#source('get_dbtables.R',chdir = T)
-#source('generate_filename.R',chdir = T)
 #prevent displaying warning messages from script on console(errors will still show)
+
+# clear workspace
+rm(list=ls())
+
 options(warn=-1)
 #suppress messages when loading package
 suppressMessages(library(timeDate))
-library(plyr)
-#suppress messages when loading package
+
+suppressMessages(library(tidyr))
+suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
-library(RSQLite)
+
+# load new packages for kolibri data extraction
+suppressMessages(library(tools))
+suppressMessages(library(gsubfn))
+
+# load postgresql library
+suppressMessages(library(DBI))
+suppressMessages(library(RPostgreSQL))
+suppressMessages(library(stringr))
 
 
-# connect to KA database 
-sqlite <- dbDriver("SQLite")
-dbfile <- "~/.kalite/database/data.sqlite"
-conn <- dbConnect(sqlite, dbfile)
+# helper function to get last name
+get_last_name <- function(full_name) {
+  # if the full name is blank or there is only one name
+  if(nchar(full_name) == 0 || length(strsplit(full_name,' ')[[1]]) == 1){
+    last_name <- ''  
+  }
+  
+  else{
+    last_name <- paste(strsplit(full_name,' ')[[1]][-1],collapse = ' ')
+  }
+  return(last_name)
+}
 
-#get users
-users_query <- dbSendQuery(conn,"SELECT * FROM securesync_facilityuser where(deleted == 0 and is_teacher == 0)")
-  #filter out deleted users and coaches, then select only columns needed for joins later in script
-users <- dbFetch(users_query) %>% select(id,username,first_name,last_name,group_id,facility_id)
+# helper function to get first name
+get_first_name <- function(full_name) {
+  
+  if(nchar(full_name) == 0 || length(strsplit(full_name,' ')[[1]]) == 1){
+    first_name <- ''  
+  }
+  else{
+    first_name <- paste(strsplit(full_name,' ')[[1]][1],collapse = ' ')
+  }
+  return(toString(first_name))
+}
 
-#get facilities - All centres have only 1 facility on centralserver except CI
-facility_query <- dbSendQuery(conn,"SELECT * FROM securesync_facility")
-  #
-facilities <- dbFetch(facility_query) %>% select(id,name)
 
-#get main_videolog
-groups_query <- dbSendQuery(conn,"SELECT * FROM securesync_facilitygroup")
-groups <- dbFetch(groups_query) %>% select(id,name,facility_id)
+# Get database credentials from environment variables
+db_name = Sys.getenv("KOLIBRI_DATABASE_NAME")
+db_host = Sys.getenv("KOLIBRI_DATABASE_HOST")
+db_user = Sys.getenv("KOLIBRI_DATABASE_USER")
+db_passwd = Sys.getenv("KOLIBRI_DATABASE_PASSWORD")
+db_port = Sys.getenv("KOLIBRI_DATABASE_PORT")
 
-#get main_userlogsummary - The most important table for month end reports. Contains user login time for each login
-ulogsummary_query <- dbSendQuery(conn,"SELECT * FROM main_userlogsummary")
-main_userlogsummary <- dbFetch(ulogsummary_query) %>% filter(deleted == 0)
+# connect to Kolibri database 
+pg <- dbDriver("PostgreSQL")
+conn <-  dbConnect(pg, dbname=db_name, host = db_host, port = db_port, user=db_user, password=db_passwd)
 
-#get main_userlog
-ulog_query <- dbSendQuery(conn,"SELECT * FROM main_userlog")
-main_userlog <- dbFetch(ulog_query)
+#facilityysers
+facilityusers <- dbGetQuery(conn,"SELECT * FROM kolibriauth_facilityuser")
 
-#get main_exerciselog
-elog_query <- dbSendQuery(conn,"SELECT * FROM main_exerciselog")
-main_exerciselog <- dbFetch(elog_query) %>% filter(deleted == 0)
+#collections
+collections <- dbGetQuery(conn,"SELECT * FROM kolibriauth_collection")
 
-#get main_videolog
-vlog_query <- dbSendQuery(conn,"SELECT * FROM main_videolog")
-main_videolog <- dbFetch(vlog_query) %>% filter(deleted == 0)
+#memberships
+memberships <- dbGetQuery(conn,"SELECT * FROM kolibriauth_membership")
 
-#get device name
-#device name derived by getting id of own device from metadata, then joining to devices config table
-device_query <- dbSendQuery(conn,"SELECT * FROM securesync_device")
-device <- dbFetch(device_query) %>% select(id,name)
+#roles
+roles <- dbGetQuery(conn,"SELECT * FROM kolibriauth_role")
 
-meta_query <- dbSendQuery(conn,"SELECT * FROM securesync_devicemetadata")
-device_meta <- dbFetch(meta_query) %>% select(id,device_id,is_own_device)
 
-device_name <- device_meta %>% filter(is_own_device == 1) %>% left_join(device,by=c("device_id" = "id"))
-device_name <- substring(device_name$name,1,3)
+#get the default facility id and from it get the device name(facility name)
+default_facility_id <- dbGetQuery(conn,"SELECT default_facility_id FROM device_devicesettings")
+
+# get module for each channel
+channel_module <- dbGetQuery(conn,"select * from channel_module")
+
+#content session logs
+content_sessionlogs <- dbGetQuery(conn,"select * from logger_contentsessionlog")
+
+#get channel content
+channel_contents <- dbGetQuery(conn,"select * from content_contentnode")
+
+channel_metadata <- dbGetQuery(conn,"select * from content_channelmetadata")
 
 #clean up and close database connection
 dbDisconnect(conn)
+
+
+#get the default facility id and from it get the device name(facility name)
+default_facility_id <- default_facility_id$default_facility_id
+
+facility_name <- collections %>% filter(id == default_facility_id) %>% select(name)
+device_name <- facility_name$name
+
+
+#filter out admins and coaches to get list of users
+# select only the relevant columns
+users <- facilityusers %>% filter(!id %in% roles$user_id) %>% select(id,full_name,username,date_joined,last_login)
+
+#join collections to memberships. (used for getting user groups)
+memberships <- memberships %>% left_join(collections,by=c("collection_id"= "id"))
+
+# get dataframe containing learners and groups they belong to
+learners_and_groups <- memberships %>% filter(kind == 'learnergroup') %>% distinct(user_id,.keep_all = TRUE) %>% select(c(name,user_id))
+
+# select only the name of the group and the user_id
+learners_and_groups <- learners_and_groups %>% select(c(name,user_id)) %>% rename(group = name)
+
+#create column in content_sessionlogs of start_timestamp with date only which will be used to get number of logins
+content_sessionlogs <- content_sessionlogs %>% mutate(start_date_only = strftime(start_timestamp,"%Y-%m-%d"))
+
+#join channel metadata to channel_module
+channel_metadata <- channel_metadata %>% left_join(channel_module,by=c("id" = "channel_id"))
+# create new column with module and abbreviated playlist name
+channel_metadata <- channel_metadata %>% mutate(abbr_name = paste(module,'_',abbreviate(name)))
+
+#create new column with abbr name and the word progress which will be used as the column name for channel progress in final report
+channel_metadata <- channel_metadata %>% mutate(abbr_name_progress = paste(abbr_name,'_progress'))
+
+#create named vector with channel_ids and abbreviated playlist names
+course_name_id <- unlist(channel_metadata$abbr_name)
+names(course_name_id) <- unlist(channel_metadata$id)
+
+#create named vector with abbr_name_progress and make the channel ids the names of each of the elements
+course_name_id_progress <- unlist(channel_metadata$abbr_name_progress)
+names(course_name_id_progress) <- unlist(channel_metadata$id)
+
+#get number of content items by channel.used to compute overall progress in channel
+#filter 
+num_contents_by_channel <- channel_contents %>% filter(!kind %in% c('topic','channel')) %>% group_by(channel_id) %>% summarise(total_items = n())
+
 
 # Simple function to generate filename of csv report in desired format
 generate_filename <- function(report,date){
@@ -88,6 +162,10 @@ alldata <- function(year_month) {
   rpt <- complete_summary %>% select(user_id,first_name,last_name,username,name.y,total_logins,total_hours,total_exercises,total_videos,month_end,exercises_attempted,last_active_date) %>% rename(group = name.y, id = user_id) %>% mutate(centre = rep(device_name), month_active = ifelse(total_hours>0, 1, 0), module=rep("numeracy"))
    # Set total exercises and total videos to 0 if total hours is 0
   rpt <- rpt %>% mutate(total_exercises=replace(total_exercises, total_hours == 0, 0)) %>% mutate(total_videos=replace(total_videos, total_hours == 0, 0))
+
+  # convert id column from uuid to character string
+  rpt <- rpt %>% mutate(id = str_replace_all(id,'-',''))
+
   # create csv file with filename generated by based on name of report and user input
   write.csv(rpt, file = generate_filename("data.all_",year_month) ,col.names = FALSE, row.names = FALSE,na="0")
   #print success messgae
