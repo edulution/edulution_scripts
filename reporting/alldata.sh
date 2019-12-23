74e0760f4bc2ce5c7516238b331b224b0b74f6d3
Kapya <kapsakala@gmail.com>
2019-12-23 16:53:06 +0200

    pull from Zambia branch when script is invoked

--- reporting/alldata.sh
+++ reporting/alldata.sh
@@ -25,8 +25,8 @@ export exit_bold_mode=`tput rmso`
 
 #pull latest changes from master branch in repo
 cd ~/.scripts
-git reset --hard origin/kolibri > /dev/null
-git pull > /dev/null
+git reset --hard origin/zambia > /dev/null
+git pull origin zambia > /dev/null
 
 # Do silent upgrade of all scripts
 ./upgrade_silent.sh
