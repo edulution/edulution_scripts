c89e5e8f3a8c8fedf338d93899cc527e7c28acdc
Kapya <kapsakala@gmail.com>
2020-01-31 13:22:44 +0200

    use complete path to bashrc

--- config/increase_session_timeout.sh
+++ config/increase_session_timeout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Increase idle session timeout to 15 mins
-sudo sed -i 's/KOLIBRI_SESSION_TIMEOUT=720/KOLIBRI_SESSION_TIMEOUT=900/g' .bashrc
+sudo sed -i 's/KOLIBRI_SESSION_TIMEOUT=720/KOLIBRI_SESSION_TIMEOUT=900/g' ~/.bashrc
