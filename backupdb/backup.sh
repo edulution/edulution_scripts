560d1f3f48f8ea5c72f7b35afff1946575d41328
Kapya <kapsakala@gmail.com>
2020-02-26 13:55:53 +0200

    slight changes to comments

--- backupdb/backup.sh
+++ backupdb/backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# file extension for backup files is custom format with extension .backup
+# File extension for backup files is custom format with extension .backup
 file_extension=".backup"
 
 # Get today's date and use it as timestamp for database backup file
 timestamp=$(date +"%Y%m%d")
 
-# get database name using direct query on terminal. remove leading and trailing whitespace
+# Get database name using direct query on terminal. remove leading and trailing whitespace
 database_name="$(PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD psql -d $KOLIBRI_DATABASE_NAME -U $KOLIBRI_DATABASE_USER -h $KOLIBRI_DATABASE_HOST -p $KOLIBRI_DATABASE_PORT -t -c 'select name from kolibriauth_collection where id = (select default_facility_id from device_devicesettings);' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
 # Derive backup name by combining the dataabse name, timestamp and file extension
@@ -15,7 +15,7 @@ kolibri_backup_name=${database_name}_kolibri_${timestamp}${file_extension}
 # name of baseline backup
 baseline_backup_name=${database_name}_baseline_${timestamp}${file_extension}
 
-# name of zip file
+# Name of zip file
 zip_file_name=${database_name}_backups_${timestamp}
 
 # Create database backup using credentials from environment variables 
@@ -29,7 +29,7 @@ echo "Creating zip file with backups"
 # create zip file containing both backups taken above and remove the original files
 zip -jm ~/backups/"$zip_file_name" ~/backups/"$kolibri_backup_name" ~/backups/"$baseline_backup_name"
 
-#remove spaces from names of backups
+# Remove spaces from names of backups
 rename "s/ //g" ~/backups/*.backup
 
 # Call script to remove backups older than 40 days
