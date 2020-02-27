c22f7df0a196cc36eb7d09cca7bc96ca9208d1b5
Kapya <kapsakala@gmail.com>
2020-02-27 11:16:58 +0200

    fixed code smells

--- ssh_tunnel/create_ssh_tunnel.sh
+++ ssh_tunnel/create_ssh_tunnel.sh
@@ -2,7 +2,7 @@
 
 # Create reverse ssh tunnel to Edulution cloud server
 createTunnel() {
-	if sshpass -p $SSHPASS /usr/bin/ssh -N -R 19999:localhost:22 edulution@130.211.93.74; then
+	if sshpass -p "$SSHPASS" /usr/bin/ssh -N -R 19999:localhost:22 edulution@130.211.93.74; then
 		echo Tunnel to Edulution created successfully
 	else
 		echo An error occurred creating a tunnel to Edulution
