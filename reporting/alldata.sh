6aa3722e6d92b5f7bcf075f9dba692b547bc8e80
Kapya <kapsakala@gmail.com>
2020-02-27 16:13:14 +0200

    remove depricated egrep and backticks

--- reporting/alldata.sh
+++ reporting/alldata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #pull latest changes from master branch in repo
-cd ~/.scripts
+cd ~/.scripts || exit
 git reset --hard origin/zambia > /dev/null
 git pull origin zambia > /dev/null
 
@@ -9,17 +9,17 @@ git pull origin zambia > /dev/null
 ./upgrade_silent.sh
 
 # check if postgresql is running before attempting to extract a report
-ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
-result=$(echo $ps_out | grep "$1")
+ps_out=$(ps -ef | grep "$1" | grep -v 'grep' | grep -v "$0")
+result=$(echo "$ps_out" | grep "$1")
 
 if [[ "$result" != "" ]];then
-	if (echo $1 |\
-    egrep '^(1[0-2]|0[0-9])[-/][0-9]{2}' > /dev/null
+	if (echo "$1" |\
+    grep -E '^(1[0-2]|0[0-9])[-/][0-9]{2}' > /dev/null
 	); then
 	   echo Stopping ka lite server 
 	   sudo service ka-lite stop > /dev/null
 	   sudo service nginx stop > /dev/null
-       echo "${green}Extracting all data until $1${reset}"
+       echo "${GREEN}Extracting all data until $1${RESET}"
        #echo Checking and fixing students with abnormal hours
        #~/.scripts/reporting/fix_crazy/fixcrazy
        echo Beginning report extraction.....
@@ -28,10 +28,10 @@ if [[ "$result" != "" ]];then
        # After Rscript executes, execute send report script
        ~/.scripts/reporting/send_report.sh
    else 
-       echo "${red}${bold}Please enter a valid month and year e.g 02-17${reset}"
+       echo "${RED}${BOLD}Please enter a valid month and year e.g 02-17${RESET}"
        exit 1 
    fi
 else
-	echo "${red}${bold}Error. Report NOT extracted. Please contact tech support 1>&2${reset}"
+	echo "${RED}${BOLD}Error. Report NOT extracted. Please contact tech support 1>&2${RESET}"
 	exit 1
 fi
--- reporting/monthend.sh
+++ reporting/monthend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #pull latest changes from master branch in repo
-cd ~/.scripts
+cd ~/.scripts || exit
 git reset --hard origin/zambia > /dev/null
 git pull origin zambia > /dev/null
 
@@ -10,8 +10,8 @@ git pull origin zambia > /dev/null
 
 # check if postgresql is running before attempting to extract a report
 function check_process_running () {
-ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
-result=$(echo $ps_out | grep "$1")
+ps_out=$(ps -ef | grep "$1" | grep -v 'grep' | grep -v "$0")
+result=$(echo "$ps_out" | grep "$1")
 if [[ "$result" != "" ]];then
     echo "Running"
 else
@@ -23,13 +23,13 @@ fi
 psql_running=$( check_process_running postgresql )
 
 if [[ "$psql_running" == 'Running' ]];then
-	if (echo $1 |\
-    egrep '^(1[0-2]|0[0-9])[-/][0-9]{2}' > /dev/null
+	if (echo "$1" |\
+    grep -E '^(1[0-2]|0[0-9])[-/][0-9]{2}' > /dev/null
 	); then
 	   echo Stopping Kolibri server 
 	   python -m kolibri stop > /dev/null
 	   sudo service nginx stop > /dev/null
-       echo "${green}Extracting data for month $1${reset}"
+       echo "${GREEN}Extracting data for month $1${RESET}"
 
        echo Beginning report extraction.....
        # fetch the first argument given on the command line and use it as an argument to the Rscript
@@ -38,16 +38,16 @@ if [[ "$psql_running" == 'Running' ]];then
        ~/.scripts/reporting/send_report.sh
        
        # submit baseline tests for the selected month
-       ~/.baseline_testing/scripts/reporting/baseline.sh $1
+       ~/.baseline_testing/scripts/reporting/baseline.sh "$1"
 
        # Pull latest changes to baseline system
        ~/.scripts/upgrade_baseline.sh
        
    else 
-       echo "${red}Please enter a valid year and month e.g 02-17${reset}"
+       echo "${RED}Please enter a valid year and month e.g 02-17${RESET}"
        exit 1 
    fi
 else
-	echo "${red}${bold}Error. Report NOT extracted. Please contact tech support ${reset}" 1>&2
+	echo "${RED}${BOLD}Error. Report NOT extracted. Please contact tech support ${RESET}" 1>&2
 	exit 1
 fi
