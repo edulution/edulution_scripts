e3ef29bb58392262cc27b3b539883975daad9706
Kapya <kapsakala@gmail.com>
2020-02-27 16:11:20 +0200

    use exported capitalized vars

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ for DIRECTORY in "${DIRECTORIES[@]}"; do
 	if [ ! -d "$DIRECTORY" ]; then
 		mkdir "$DIRECTORY"
 	else
-		echo "${blue}$DIRECTORY already exists. Skipping this step${reset}"
+		echo "${BLUE}$DIRECTORY already exists. Skipping this step${RESET}"
 	fi
 done
 
@@ -15,41 +15,41 @@ cd ~ || exit
 
 #If bash aliases already exists, replace it with latest version. If not, create it
 if test -f ~/.bash_aliases; then
-	echo "${blue}Bash aliases file already exists. Replacing with latest version${reset}"
+	echo "${BLUE}Bash aliases file already exists. Replacing with latest version${RESET}"
 	sudo rm .bash_aliases
 	sudo cp .scripts/.bash_aliases ~
 else
-	echo "${blue}${bold}Aliases file does not exist. Inserting latest version${reset}"
+	echo "${BLUE}${BOLD}Aliases file does not exist. Inserting latest version${RESET}"
 	sudo cp ~/.scripts/.bash_aliases ~
 fi
 
 #If bash colors already exists, replace it with latest version. If not, create it
 if test -f ~/.bash_colors; then
-	echo "${blue}Bash aliases file already exists. Replacing with latest version${reset}"
+	echo "${BLUE}Bash aliases file already exists. Replacing with latest version${RESET}"
 	sudo rm .bash_colors
 	sudo cp .scripts/.bash_colors ~
 else
-	echo "${blue}${bold}Aliases file does not exist. Inserting latest version${reset}"
+	echo "${BLUE}${BOLD}Aliases file does not exist. Inserting latest version${RESET}"
 	sudo cp ~/.scripts/.bash_colors ~
 fi
 
 #test if upgrade script exists. If not add it
 if test -f ~/upgrade; then
-	echo "${blue}Upgrade script already exists. Replacing with latest version${reset}"
+	echo "${BLUE}Upgrade script already exists. Replacing with latest version${RESET}"
 	sudo rm upgrade
 	sudo cp .scripts/upgrade ~
 else
-	echo "${blue}${bold}Upgrade script does not exist. Inserting it now${reset}"
+	echo "${BLUE}${BOLD}Upgrade script does not exist. Inserting it now${RESET}"
 	sudo cp ~/.scripts/upgrade ~
 fi
 
 # check if postgresql is installed. Alert user to contact support if it is not installed
 if [ "$(dpkg-query -W -f='${Status}' postgresql 2>/dev/null | grep -c 'ok installed')" -eq 0 ];
 then
-  echo "${red}${bold}PostgreSQL is not installed. Please contact support${reset}"
+  echo "${RED}${BOLD}PostgreSQL is not installed. Please contact support${RESET}"
   
 else
-  echo "${blue}PostgreSQL is already installed. Skipping..${reset}"
+  echo "${BLUE}PostgreSQL is already installed. Skipping..${RESET}"
 fi
 
 # Run backup script
@@ -62,14 +62,14 @@ touch ~/.reports/test.R
 ~/.scripts/identify/whoru >> ~/.reports/test.R
 
 # Test the report submission by sending the test file
-echo "${white}${bold}Testing report submission...${reset}"
+echo "${WHITE}${BOLD}Testing report submission...${RESET}"
 
 # if connection lost the script will exit with status 1 and output error message
 if sshpass -p "$SSHPASS" scp ~/.reports/test.R edulution@130.211.93.74:/home/edulution/reports; then
-	echo "${green}${bold}Report submitted successfully!${reset}"
-	echo "${green}${bold}Scripts have been set up correctly${reset}"
+	echo "${GREEN}${BOLD}Report submitted successfully!${RESET}"
+	echo "${GREEN}${BOLD}Scripts have been set up correctly${RESET}"
 else
-	echo "${red}${bold}Something went wrong or internet connection was lost${reset}" 1>&2
+	echo "${RED}${BOLD}Something went wrong or internet connection was lost${RESET}" 1>&2
 	exit 1
 fi
 
