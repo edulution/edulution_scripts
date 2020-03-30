e82c7fae4e613539c0313d2ce448bf4ea8097059
Kapya <kapsakala@gmail.com>
2020-03-31 01:35:59 +0200

    (untested) refactor as recommended by shellcheck

--- reporting/send_report.sh
+++ reporting/send_report.sh
@@ -5,7 +5,7 @@ cd ~/.reports || exit
 #check reports folder for file most recently created from monthend or alldata and send to google server
 
 # if connection lost the script will exit with status 1 and output error message
-if sshpass -p "$SSHPASS" scp `ls ~/.reports -t | head -n1` edulution@130.211.93.74:/home/edulution/reports; then
+if sshpass -p "$SSHPASS" scp "$( ls ~/.reports -t | head -n1 )" edulution@130.211.93.74:/home/edulution/reports; then
 	echo "${GREEN}${BOLD}Report submitted successfully!${RESET}"
 else
 	echo "${RED}${BOLD}Report not submitted. Please check your internet connection and try again 1>&2${RESET}"
