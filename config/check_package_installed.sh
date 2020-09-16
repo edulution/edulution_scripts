b7001f2386132207cf45a2ba7198695f5c87eb64
Kapya <kapsakala@gmail.com>
2020-09-16 02:25:43 +0200

    improved comments

--- config/check_package_installed.sh
+++ config/check_package_installed.sh
@@ -3,10 +3,9 @@
 check_package_installed(){
 	package_name=$1
 
-	# check if postgresql is installed. Alert user to contact support if it is not installed
+	# Check if a package is installed and inform the user
 	# shellcheck disable=SC2086
 	if [ "$(which $package_name)" != "" ]
-	# if [ "$(dpkg -l | grep "$package_name")" -eq 0 ];
 	then
 	  echo "${BLUE}$package_name is already installed.${RESET}"
 	  
