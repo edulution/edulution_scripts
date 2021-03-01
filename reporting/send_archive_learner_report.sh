a3b0369abcad9814d9ce0b059563c06ea73e755b
sjtg <chipilej@gmail.com>
2021-03-01 10:28:24 +0200

    Archive Learners

---
+++ reporting/send_archive_learner_report.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Go into reports folder
+cd ~/Archive_Learner || exit
+
+#check reports folder for file most recently created from monthend or alldata and send to google server
+MOST_REC_FILEPATH=$( ls ~/Archive_Learner -t | head -n1 )
+
+# Get the name of the most recent file without path and extension
+MOST_REC_FILENAME=$( basename "$MOST_REC_FILEPATH" .csv)
+
+# Create a zip file with this file in the current directory
+# Expected output is $MOST_REC_FILENAME.zip and the original csv file deleted
+zip -jm "$MOST_REC_FILENAME" "$MOST_REC_FILEPATH" 
+
+# if connection lost the script will exit with status 1 and output error message
+# if sshpass -p "$SSHPASS" scp "$MOST_REC_FILENAME.zip" edulution@130.211.93.74:/home/edulution/reports; then
+# 	echo "${GREEN}${BOLD}Report submitted successfully!${RESET}"
+# else
+# 	echo "${RED}${BOLD}Report not submitted. Please check your internet connection and try again 1>&2${RESET}"
+# 	exit 1
+# fi
