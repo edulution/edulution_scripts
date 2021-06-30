9d62c5123412b4d88c8e5163e32e52c4fa3d4545
Kapya Sakala <kapsakala@gmail.com>
2021-06-30 06:09:17 +0200

    changed branch to zambia

--- upgrade_silent.sh
+++ upgrade_silent.sh
@@ -43,11 +43,11 @@ fi
 
 # Check if kolibri helper scripts directory exists. pull it if it does not
 if [ -d "$kolibri_helper_scripts_dir" ]; then
-	cd $kolibri_helper_scripts_dir && git reset --hard origin/south_africa && git pull origin south_africa && cd ~ || exit
+	cd $kolibri_helper_scripts_dir && git reset --hard origin/zambia && git pull origin zambia && cd ~ || exit
 else
 	echo "Helper scripts directory does not exist. Cloning now..."
 	git clone https://github.com/edulution/kolibri_helper_scripts.git $kolibri_helper_scripts_dir
-	cd $kolibri_helper_scripts_dir && git reset --hard origin/south_africa && git pull origin south_africa && cd ~ || exit
+	cd $kolibri_helper_scripts_dir && git reset --hard origin/zambia && git pull origin zambia && cd ~ || exit
 fi
 
 # Run backup script
