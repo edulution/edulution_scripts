6eb755cad6806a1b9b859e4c2d2ab0f62c96022c
Kapya Sakala <kapsakala@gmail.com>
2021-08-20 16:42:16 +0200

    removed activity deletion from silent upgrade

--- upgrade_silent.sh
+++ upgrade_silent.sh
@@ -61,8 +61,5 @@ fi
 ###################
 ~/.scripts/config/add_pgtune_settings.sh
 
-# Delete learner activity for all learners except grade 7
-python ~/.kolibri_helper_scripts/delete_activity_EZL.py
-
 # Do channel subscriptions 
 ~/.kolibri_helper_scripts/channel_setup/insert_channel_subscriptions.sh
