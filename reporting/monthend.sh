c955477e5a06b49bfcf8e0cb996f8552802c5fd3
Kapya Sakala <kapsakala@gmail.com>
2021-11-15 16:45:09 +0200

    delete any loose csv files in the reports directory before beginning report extraction

--- reporting/monthend.sh
+++ reporting/monthend.sh
@@ -34,6 +34,10 @@ if [[ "$psql_running" == 'Running' ]];then
 	   sudo service nginx stop > /dev/null
        echo "${GREEN}Extracting data for month $1${RESET}"
 
+       # Delete any loose csv files in the reports directory before extraction
+       cd ~/.reports
+       find . -type f \( -name "*.csv" \) -exec rm {} \;
+
        echo Beginning report extraction.....
        # fetch the first argument given on the command line and use it as an argument to the Rscript
        cd ~/.scripts/reporting || exit
