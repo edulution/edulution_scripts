9915407fdb13b74abbc77f743a2797871b1652ed
Ntipa Chola <sword@DESKTOP-NT5NC6Q>
2023-01-18 17:22:43 +0200

    Added help option to script

--- config/check_or_create_dirs.sh
+++ config/check_or_create_dirs.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "NAME"
+  echo "  check_or_create_dirs - Check or create directories"
+  echo
+  echo "DESCRIPTION"
+  echo "	This script contains a function 'check_or_create_dirs' which is used to check and create directories specified as arguments."
+  echo "	The function takes any number of directory paths as arguments and checks if they exist, if not it creates them."
+  echo "	The function also prints messages to inform if a directory already exists or was created."
+  exit 1
+fi
+
 check_or_create_dirs(){
 	DIRECTORIES=("$@")
 	for DIRECTORY in "${DIRECTORIES[@]}"; do
