f8ad1af0c958c02e0f88a819b2418e1edab76508
Ntipa Chola <sword@DESKTOP-NT5NC6Q>
2023-01-20 08:23:22 +0200

    Added help option to script

--- config/reinstall_dbhelpers_package.sh
+++ config/reinstall_dbhelpers_package.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "NAME"
+  echo "  reinstall_dbhelpers_package - Reinstall dbhelpers R package"
+  echo
+  echo "DESCRIPTION"
+  echo "	This script installs the 'dbhelpers' R package from the 'Aypak' GitHub repository using 'devtools' package."
+  exit 1
+fi
+
 sudo su - -c "R -e \"devtools::install_github('Aypak/dbhelpers', upgrade = 'never')\""
