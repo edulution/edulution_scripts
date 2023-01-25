24c82810957234e365706bcef036a6a5c04437c2
Kapya Sakala <kapsakala@gmail.com>
2023-01-25 14:53:46 +0200

    refactored to return exit code

--- gui/functions.sh
+++ gui/functions.sh
@@ -11,13 +11,7 @@ check_internet_connection() {
     # Check if there is an internet connection
     wget -q --tries=10 --timeout=20 --spider http://google.com
 
-    if [[ $? -eq 0 ]]; then
-        # Connection is successful
-        zenity --info --title="Internet connection" --text="You are connected to the internet."
-        kill $ZENITY_PID
-    else
-        # Connection is not successful
-        zenity --error --title="Internet connection" --text="You are not connected to the internet."
-        kill $ZENITY_PID
-    fi
+    local exit_code=$?
+    kill $ZENITY_PID
+    return $exit_code
 }
