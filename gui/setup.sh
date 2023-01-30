2c9323a2625dafa2e1d7b581245c3ccd473f14ba
Kapya Sakala <kapsakala@gmail.com>
2023-01-30 15:56:28 +0200

    use dirname -bash to get directory where function is running

--- gui/setup.sh
+++ gui/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # System-wide installation
-sudo cp ~/.scripts/gui/edulution.desktop /usr/share/applications/
+sudo cp "$(dirname "$0")"/edulution.desktop /usr/share/applications/
 
 # Installation for current user
-cp ~/.scripts/gui/edulution.desktop ~/.local/share/application
+cp "$(dirname "$0")"/edulution.desktop ~/.local/share/application
 
 # Pin the edulution desktop entry to the taskbar launcher
 gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites | sed "s/]/, 'edulution.desktop']/")"
