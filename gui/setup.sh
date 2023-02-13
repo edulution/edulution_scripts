91d63f0ac339abda3ec50afc9b99f8f1b9935f98
Kapya Sakala <kapsakala@gmail.com>
2023-02-13 14:05:22 +0200

    check or create reports dir

--- gui/setup.sh
+++ gui/setup.sh
@@ -7,7 +7,7 @@ source $(dirname "${BASH_SOURCE[0]}")/functions.sh
 CURRENT_DIR=$(dirname "$0")
 
 # Create error log directory
-DIRECTORIES=( ~/.logs ~/backups )
+DIRECTORIES=( ~/.logs ~/.reports ~/backups )
 
 # Create the logs directory if it does not exist
 check_or_create_dirs "${DIRECTORIES[@]}"
