0dbd3b37f6e5b7739b3fc166fbb8a25c350acc87
Kapya Sakala <kapsakala@gmail.com>
2023-02-13 14:02:11 +0200

    source functions script. create logs directory

--- gui/setup.sh
+++ gui/setup.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Source the script containg major functions
+source $(dirname "${BASH_SOURCE[0]}")/functions.sh
+
+# Get the current directory
+CURRENT_DIR=$(dirname "$0")
+
+# Create error log directory
+DIRECTORIES=( ~/.logs )
+
+# Create the logs directory if it does not exist
+check_or_create_dirs "${DIRECTORIES[@]}"
+
 # System-wide installation
-sudo cp "$(dirname "$0")"/edulution.desktop /usr/share/applications/
+sudo cp "$CURRENT_DIR"/edulution.desktop /usr/share/applications/
 
 # Installation for current user
-cp "$(dirname "$0")"/edulution.desktop ~/.local/share/application
+cp "$CURRENT_DIR"/edulution.desktop ~/.local/share/application
 
 # Pin the edulution desktop entry to the taskbar launcher
 gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites | sed "s/]/, 'edulution.desktop']/")"
