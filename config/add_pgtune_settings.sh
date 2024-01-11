13510eefde67b111982d32ba07ec9ac0a4011824
Kapya Sakala <kapsakala@gmail.com>
2024-01-11 10:30:02 +0200

    modify pgtune settings for postgres 14, SSD storage

--- config/add_pgtune_settings.sh
+++ config/add_pgtune_settings.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-DIRECTORY="/etc/postgresql/13/main"
+DIRECTORY="/etc/postgresql/14/main"
 
 # Store path to conf file in variable
 CONF_FILE="$DIRECTORY/postgresql.conf"
 
 if [ ! -d "$DIRECTORY" ]; then
-	echo "Postgres 13 has not been set up. Skipping...."
+	echo "Postgres 14 has not been set up. Skipping...."
 else
-	echo "Postgres 13 has been set up. Adding tuning settings"
+	echo "Postgres 14 has been set up. Adding tuning settings"
 
 	# Get backup of postgresql conf file
 	# Remove all lines after the Add settings for extensions here line
 	sudo sed -i.backup '1,/Add settings for extensions here/!d' "$CONF_FILE"
 
-	# Add the settings for 2 cores, 4GB RAM, HDD storage, data warehouse
+	# Add the settings for 2 cores, 4GB RAM, SSD storage, data warehouse
 
 	echo "shared_preload_libraries = 'pg_stat_statements'" | sudo tee -a "$CONF_FILE"
 	echo "max_connections = 200" | sudo tee -a "$CONF_FILE"
@@ -24,15 +24,11 @@ else
 	echo "checkpoint_completion_target = 0.9" | sudo tee -a "$CONF_FILE"
 	echo "wal_buffers = 16MB" | sudo tee -a "$CONF_FILE"
 	echo "default_statistics_target = 500" | sudo tee -a "$CONF_FILE"
-	echo "random_page_cost = 4" | sudo tee -a "$CONF_FILE"
-	echo "effective_io_concurrency = 2" | sudo tee -a "$CONF_FILE"
-	echo "work_mem = 13107kB" | sudo tee -a "$CONF_FILE"
+	echo "random_page_cost = 1.1" | sudo tee -a "$CONF_FILE"
+	echo "effective_io_concurrency = 200" | sudo tee -a "$CONF_FILE"
+	echo "work_mem = 1310kB" | sudo tee -a "$CONF_FILE"
 	echo "min_wal_size = 4GB" | sudo tee -a "$CONF_FILE"
 	echo "max_wal_size = 16GB" | sudo tee -a "$CONF_FILE"
-	echo "max_worker_processes = 2" | sudo tee -a "$CONF_FILE"
-	echo "max_parallel_workers_per_gather = 1" | sudo tee -a "$CONF_FILE"
-	echo "max_parallel_workers = 2" | sudo tee -a "$CONF_FILE"
-	echo "max_parallel_maintenance_workers = 1" | sudo tee -a "$CONF_FILE"
 
 	# restart the postgresql service
 	sudo systemctl restart postgresql
